27767f5475ea9b1d045a4e53e3298c66f5007578
tsao <tsaoyuan@gmail.com>
2022-08-13 16:08:36 +0800

    [Mike Dane] Create GetMax Method
    - if..else..
    - operator
    - have return

---
+++ CH03/Max/Max.cs
@@ -0,0 +1,20 @@
+﻿static int GetMax(int num1, int num2, int num3)
+{
+    int result;
+    if (num1 > num2 && num1 > num3)
+    {
+        result = num1;
+    }
+    else if (num1 < num2 && num2 > num3)
+    {
+        result = num2;
+    }
+    else 
+    {
+        result = num3;
+    }
+    
+    return result;
+}
+
+Console.WriteLine(GetMax(6, 50, 12)); 
