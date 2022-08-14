195fe089c412214c68d7f5f5128966ab4cc2071e
tsao <tsaoyuan@gmail.com>
2022-08-14 17:19:10 +0800

    [Mike Dane] Create tryCatch Method
    - 分母為零, 執行除法會出錯
    - 出錯時, 用 Try Catch 捕捉例外狀況
    - Exception e, 把例外狀況印出

---
+++ CH03/tryCatch/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.Write("Enter first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(num1 / num2);
+
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Error {e.Message}");
+}
